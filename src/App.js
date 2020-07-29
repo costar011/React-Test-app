1f178c7ff5601de896d4b7eca895a63833259021
YERIM <dnaer456@gmail.com>
2020-07-29 18:40:26 +0900

    css

--- src/App.js
+++ src/App.js
@@ -5,17 +5,11 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <div className="App__top">
-          <div className="App__top__box">top box</div>
-        </div>
+        <div className="App__top"></div>
 
-        <div className="App__main">
-          <div className="App__main__box">main box</div>
-        </div>
+        <div className="App__main"></div>
 
-        <div className="App__bottom">
-          <div className="App__bottom__box">bottom box</div>
-        </div>
+        <div className="App__bottom"></div>
       </div>
     );
   }
