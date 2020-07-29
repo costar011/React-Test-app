654e4c0c5c7b365ac73eef4457bb3756b830c9bb
YERIM <dnaer456@gmail.com>
2020-07-29 20:19:24 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./styles/app.css";
+import "react-daypicker/lib/DayPicker.css";
+import DayPicker from "react-daypicker";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <div className="App__top">Title</div>
-
-        <div className="App__main">Content</div>
-
-        <div className="App__bottom">dev</div>
+        <div className="App__main">
+          <DayPicker onDayClick={(day) => this.setState({ day })} />
+        </div>
       </div>
     );
   }
